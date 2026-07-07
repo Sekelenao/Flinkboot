8313b60b06a9968829c402131db8296bb2175e97
Sekelenao <Sekelenao@protonmail.com>
2026-07-07 23:42:25 +0200

    feat: implement Flinkboot initialization logic

--- flinkboot-core/src/main/java/io/github/sekelenao/api/Flinkboot.java
+++ flinkboot-core/src/main/java/io/github/sekelenao/api/Flinkboot.java
@@ -1,4 +1,22 @@
 package io.github.sekelenao.api;
 
-public class Flinkboot {
+import io.github.sekelenao.internal.command.CommandLine;
+
+import java.util.Objects;
+
+public final class Flinkboot {
+
+    private static final String CONFIG_LOCATION_PROPERTY = "flinkboot-config";
+
+    private final CommandLine commandLine;
+
+    private Flinkboot(String[] args) {
+        this.commandLine = CommandLine.parse(args);
+    }
+
+    public static Flinkboot initialize(String[] args){
+        Objects.requireNonNull(args);
+        return new Flinkboot(args);
+    }
+
 }
