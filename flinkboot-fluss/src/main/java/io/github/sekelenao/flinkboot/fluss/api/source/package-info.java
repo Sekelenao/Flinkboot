3aaffcf60c2ae303935092e201f98722ea0c0682
Sekelenao <Sekelenao@protonmail.com>
2026-09-08 23:30:54 +0200

    docs(fluss): fix Javadoc @link references to FlussSink and FlussSource

--- flinkboot-fluss/src/main/java/io/github/sekelenao/flinkboot/fluss/api/source/package-info.java
+++ flinkboot-fluss/src/main/java/io/github/sekelenao/flinkboot/fluss/api/source/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Factory utilities for constructing Apache Flink {@link org.apache.fluss.connector.flink.source.FlussSource} instances from configuration properties.
+ * Factory utilities for constructing Apache Flink {@link org.apache.fluss.flink.source.FlussSource} instances from configuration properties.
  */
 package io.github.sekelenao.flinkboot.fluss.api.source;
