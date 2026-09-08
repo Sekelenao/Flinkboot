10a26b0621aa8b3654c4fa88f7761506bdb7d878
Sekelenao <Sekelenao@protonmail.com>
2026-09-08 23:57:22 +0200

    refactor(test): remove unused org.junit.jupiter.params from module-info

--- flinkboot-test/src/main/java/module-info.java
+++ flinkboot-test/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module io.github.sekelenao.flinkboot.test {
     requires transitive io.github.sekelenao.flinkboot.core;
     requires transitive org.junit.jupiter.api;
-    requires org.junit.jupiter.params;
 
     exports io.github.sekelenao.flinkboot.test.api;
     exports io.github.sekelenao.flinkboot.test.api.assertion;
