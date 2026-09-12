c1e241fb9c7500da7adf913595552357416df69f
pallavi hegde <86112546+Pallavi-p-h@users.noreply.github.com>
2026-09-12 07:53:24 +0530

    Refactor: Remove internal OffsetInitializerProperties leak

--- flinkboot-kafka/src/main/java/io/github/sekelenao/flinkboot/kafka/internal/OffsetInitializerMapper.java
+++ flinkboot-kafka/src/main/java/io/github/sekelenao/flinkboot/kafka/internal/OffsetInitializerMapper.java
@@ -1,6 +1,7 @@
 package io.github.sekelenao.flinkboot.kafka.internal;
 
 import io.github.sekelenao.flinkboot.kafka.api.properties.source.KafkaOffsetInitializer;
+import io.github.sekelenao.flinkboot.kafka.api.properties.source.KafkaSourceProperties;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.kafka.common.TopicPartition;
 
@@ -12,18 +13,18 @@ public final class OffsetInitializerMapper {
         throw new AssertionError("You cannot instantiate this class");
     }
 
-    public static OffsetsInitializer map(OffsetInitializerProperties configuration) {
-        var offset = configuration.startingOffsets();
+    public static OffsetsInitializer map(KafkaSourceProperties properties) {
+        var offset = properties.startingOffsets();
         if (offset == KafkaOffsetInitializer.OFFSETS) {
-            return offsetsPerPartition(configuration);
+            return offsetsPerPartition(properties);
         }
         if (offset == KafkaOffsetInitializer.TIMESTAMP) {
-            return timestampOffsets(configuration);
+            return timestampOffsets(properties);
         }
         return offset.offsetsInitializer();
     }
 
-    private static OffsetsInitializer offsetsPerPartition(OffsetInitializerProperties configuration) {
+    private static OffsetsInitializer offsetsPerPartition(KafkaSourceProperties configuration) {
         var offsetInitializerConfiguration = new HashMap<TopicPartition, Long>();
         for (var entry : configuration.startingOffsetsPartitionOffsets()) {
             var topicPartition = new TopicPartition(entry.topic(), entry.partition());
@@ -32,7 +33,7 @@ public final class OffsetInitializerMapper {
         return OffsetsInitializer.offsets(offsetInitializerConfiguration);
     }
 
-    private static OffsetsInitializer timestampOffsets(OffsetInitializerProperties configuration) {
+    private static OffsetsInitializer timestampOffsets(KafkaSourceProperties configuration) {
         return OffsetsInitializer.timestamp(configuration.startingOffsetsTimestamp().orElseThrow());
     }
 }
